5c3e0ed2705cefe36d8a6bc826b8ec09469e21c1
minjeong <cpflqhdlrmsu@gmail.com>
2020-09-13 18:43:54 +0900

    Build main view and design

---
+++ Assets/Script/gamestart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gamestart : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
